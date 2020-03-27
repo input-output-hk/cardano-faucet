58443a3b267da42d83ccb499c9667935414d211b
John Lotoski <john.lotoski@iohk.io>
2020-03-27 08:28:32 -0400

    Bump node to 1.9.0, bump wallet master and remove sep cli

--- nix/nixos/create-faucet-wallet/default.nix
+++ nix/nixos/create-faucet-wallet/default.nix
@@ -1,9 +1,9 @@
-{ runCommand, cardano-wallet-byron-cli, expect, jq }:
+{ runCommand, cardano-wallet-byron, expect, jq }:
 
 runCommand "create-faucet-wallet" {
   expect = "${expect}/bin/expect";
   jq = "${jq}/bin/jq";
-  cardanoWalletByronCli = "${cardano-wallet-byron-cli}/bin/cardano-wallet-byron";
+  cardanoWalletByron = "${cardano-wallet-byron}/bin/cardano-wallet-byron";
 } ''
   substituteAll ${./create-faucet-wallet.sh} $out
   chmod +x $out
