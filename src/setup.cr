2982e2d78a4781e913e19f95b1a09229c40fe68c
John Lotoski <john.lotoski@iohk.io>
2020-05-21 19:59:18 -0400

    Implements enforced auth mode, auth and no-auth lovelace yields

--- src/setup.cr
+++ src/setup.cr
@@ -2,7 +2,7 @@
 # - Setup and config
 #
 
-ANONYMOUS_ACCESS         = ENV.fetch("ANONYMOUS_ACCESS", "TRUE")
+ANONYMOUS_ACCESS         = ENV.fetch("ANONYMOUS_ACCESS", "TRUE") == "TRUE" ? true : false
 FAUCET_API_KEY_PATH      = ENV.fetch("FAUCET_API_KEY_PATH", "/var/lib/cardano-faucet/faucet.apikey")
 FAUCET_LOG_LEVEL         = ENV.fetch("CRYSTAL_LOG_LEVEL", "INFO")
 FAUCET_LOG_SOURCES       = ENV.fetch("CRYSTAL_LOG_SOURCES", "*")
@@ -12,7 +12,7 @@ FAUCET_WALLET_ID_PATH    = ENV.fetch("FAUCET_WALLET_ID_PATH", "/var/lib/cardano-
 LOVELACES_TO_GIVE_ANON   = ENV.fetch("LOVELACES_TO_GIVE_ANON", "1000000000").to_u64
 LOVELACES_TO_GIVE_APIKEY = ENV.fetch("LOVELACES_TO_GIVE_APIKEY", "1000000000").to_u64
 SECONDS_BETWEEN_REQUESTS = ENV.fetch("SECONDS_BETWEEN_REQUESTS", "86400").to_i
-USE_BYRON_WALLET         = ENV.fetch("USE_BYRON_WALLET", "TRUE");
+USE_BYRON_WALLET         = ENV.fetch("USE_BYRON_WALLET", "TRUE") == "TRUE" ? true : false
 WALLET_LISTEN_PORT       = ENV.fetch("WALLET_LISTEN_PORT", "8090").to_i
 WALLET_API               = ENV.fetch("WALLET_API", "http://localhost:#{WALLET_LISTEN_PORT}/v2")
 
