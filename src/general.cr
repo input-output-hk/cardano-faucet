6f01a312f3a1b80f09cc9d949472918b462c6c18
John Lotoski <john.lotoski@iohk.io>
2020-05-29 15:27:57 -0400

    Adds parsing support for the updated apikey file format

--- src/general.cr
+++ src/general.cr
@@ -11,25 +11,55 @@ def readFile(file)
 end
 
 def readKeys(file)
-  apiKeys = Hash(String, String).new
+  apiKeys = Hash(String, Hash(Symbol, String | UInt32 | UInt64)).new
   f = readFile(file)
   if f != ""
     f.split("\n").each.with_index do |i, c|
+      msgPrefix = "API Key file \"#{file}\", line \"#{c + 1}\""
+
+      # Remove comment lines and whitespace lines
       if i.lstrip =~ /^#/ || i =~ /^\s*$/
         next
       end
-      keyFields = i.split(" ")
-      if !(keyFields[0] =~ /^[A-Za-z0-9]{#{API_KEY_LEN}}$/)
-        msg = "API Key file \"#{file}\", line \"#{c + 1}\", key \"#{keyFields[0]}\" " \
-              "is not a #{API_KEY_LEN} char alphanumeric"
-        Log.error { msg }
-        raise(msg)
-      end
-      if keyFields.size == 1
-        apiKeys[keyFields[0]] = "Uncommented"
+
+      #Log.debug { "#{i.split.size} : #{i.split}" }
+
+      # Ensure all required fields are included in each key record processed
+      raise "#{msgPrefix} does not contain the 3 required fields" if i.split.size < 3
+
+      keyFields = i.split
+
+      # Ensure the key field API key is alphanumeric of API_KEY_LEN
+      if keyFields[0] =~ /^[A-Za-z0-9]{#{API_KEY_LEN}}$/
+        apiKey = keyFields[0]
       else
-        apiKeys[keyFields[0]] = keyFields[1..-1].join(" ")[0..API_KEY_COMMENT_MAX_LEN]
+        raise "#{msgPrefix}, key \"#{keyFields[0]}\" is not a #{API_KEY_LEN} char alphanumeric"
       end
+
+      # Ensure the key field LOVELACES_PER_TX is > 0 or "default" and parse
+      if (keyFields[1].to_u64? && keyFields[1].to_u64 > 0) || keyFields[1].to_s == "default"
+        lovelacesPerTx = keyFields[1].to_u64? ? keyFields[1].to_u64 : LOVELACES_TO_GIVE_APIKEY
+      else
+        raise "#{msgPrefix}, LOVELACES_PER_TX field is not > 0 or \"default\" (without quotes)"
+      end
+
+      # Ensure the key field PERIOD_PER_TX is >= 0 or "default" and parse
+      if keyFields[2].to_u64? || keyFields[2].to_s == "default"
+        periodPerTx = keyFields[2].to_u64? ? keyFields[2].to_u64 : SECS_BETWEEN_REQS_APIKEY
+      else
+        raise "#{msgPrefix}, PERIOD_PER_TX field is not >= 0 or \"default\" (without quotes)"
+      end
+
+      # Ensure the comment field, if provided is set properly
+      if keyFields.size == 3
+        comment = "Uncommented"
+      else
+        comment = keyFields[3..-1].join(" ")[0..API_KEY_COMMENT_MAX_LEN]
+      end
+
+      apiKeys[apiKey] = {:lovelacesPerTx => lovelacesPerTx,
+                         :periodPerTx => periodPerTx,
+                         :comment => comment}
     end
   end
   apiKeys
