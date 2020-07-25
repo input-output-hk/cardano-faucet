b0de43a6c3092616a5a8b08229aeb729046b0f51
John Lotoski <john.lotoski@iohk.io>
2020-07-25 17:49:16 -0400

    Adds node 1.18.0 support

--- nix/nixos/cardano-faucet-service.nix
+++ nix/nixos/cardano-faucet-service.nix
@@ -202,7 +202,7 @@ in {
       type = types.enum [ "mainnet" "staging" "testnet" ];
       default = if __elem cfg.cardanoEnv [ "mainnet" ]
                   then "mainnet"
-                  else if __elem cfg.cardanoEnv [ "mainnet_candidate" "mainnet_candidate_2" "mainnet_candidate_3" "mainnet_candidate_4" ]
+                  else if lib.hasPrefix "mainnet_candidate" cfg.cardanoEnv
                     then "staging"
                     else "testnet";
       description = "Which mode to start wallet in: --mainnet, --staging or --testnet";
