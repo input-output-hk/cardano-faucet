575fdad4ad08c57be26b06b9dc722ce63d502326
Jean-Baptiste Giraudeau <jb@giraudeau.info>
2021-09-20 14:52:12 +0200

    Use cardano-{node, cli} from wallet package.

--- nix/default.nix
+++ nix/default.nix
@@ -6,10 +6,8 @@ in with {
     inherit (import sources.niv { }) niv;
     inherit (crystalPkgs) crystal2nix expect jq pkg-config openssl shards;
     crystal = crystalPkgs.crystal_0_34;
-    inherit (import sources.cardano-node { gitrev = sources.cardano-node.rev; })
-      cardano-cli;
     inherit (import sources.cardano-wallet { gitrev = sources.cardano-wallet.rev; })
-      cardano-wallet;
+      cardano-wallet cardano-cli cardano-node;
     packages = self.callPackages ./packages.nix { };
     inherit (import sources.gitignore { inherit (self) lib; }) gitignoreSource;
     iohkNix = import sources.iohk-nix {
