0f9206ea3692fbf5d23fe739f12ad5520644c4f5
John Lotoski <john.lotoski@iohk.io>
2024-09-04 16:31:26 -0500

    fmt: add fourmolu, cfg and reformat .hs

--- cardano-faucet/src/Cardano/Faucet/TxUtils.hs
+++ cardano-faucet/src/Cardano/Faucet/TxUtils.hs
@@ -1,16 +1,49 @@
 module Cardano.Faucet.TxUtils where
 
-import Cardano.Api (ShelleyBasedEra, TxIn, TxOut(TxOut), CtxUTxO, TxBody, TxBodyContent(TxBodyContent), Witness(KeyWitness), KeyWitnessInCtx(KeyWitnessForSpending), TxInsCollateral(TxInsCollateralNone), TxInsReference(TxInsReferenceNone), TxTotalCollateral(TxTotalCollateralNone), TxReturnCollateral(TxReturnCollateralNone), TxMetadataInEra(TxMetadataNone), TxAuxScripts(TxAuxScriptsNone), TxExtraKeyWitnesses(TxExtraKeyWitnessesNone), TxWithdrawals(TxWithdrawalsNone), TxCertificates, BuildTxWith(BuildTxWith), TxUpdateProposal(TxUpdateProposalNone), TxMintValue(..), TxScriptValidity(TxScriptValidityNone), defaultTxValidityUpperBound, docToText, Tx, makeShelleyKeyWitness, makeSignedTransaction, TxId, getTxId, BuildTx, ShelleyWitnessSigningKey, AddressAny, TxValidityLowerBound (TxValidityNoLowerBound))
+import Cardano.Api (
+  AddressAny,
+  BuildTx,
+  BuildTxWith (BuildTxWith),
+  CtxUTxO,
+  KeyWitnessInCtx (KeyWitnessForSpending),
+  ShelleyBasedEra,
+  ShelleyWitnessSigningKey,
+  Tx,
+  TxAuxScripts (TxAuxScriptsNone),
+  TxBody,
+  TxBodyContent (TxBodyContent),
+  TxCertificates,
+  TxExtraKeyWitnesses (TxExtraKeyWitnessesNone),
+  TxId,
+  TxIn,
+  TxInsCollateral (TxInsCollateralNone),
+  TxInsReference (TxInsReferenceNone),
+  TxMetadataInEra (TxMetadataNone),
+  TxMintValue (..),
+  TxOut (TxOut),
+  TxReturnCollateral (TxReturnCollateralNone),
+  TxScriptValidity (TxScriptValidityNone),
+  TxTotalCollateral (TxTotalCollateralNone),
+  TxUpdateProposal (TxUpdateProposalNone),
+  TxValidityLowerBound (TxValidityNoLowerBound),
+  TxWithdrawals (TxWithdrawalsNone),
+  Witness (KeyWitness),
+  defaultTxValidityUpperBound,
+  docToText,
+  getTxId,
+  makeShelleyKeyWitness,
+  makeSignedTransaction,
+ )
 import qualified Cardano.Api.Ledger as L
-import Cardano.Api.Shelley (lovelaceToValue, Value, createAndValidateTransactionBody)
-import Cardano.Faucet.Misc (getValue, faucetValueToLovelace)
-import Cardano.Faucet.Types (FaucetWebError(..), FaucetValue)
+import Cardano.Api.Shelley (Value, createAndValidateTransactionBody, lovelaceToValue)
+import Cardano.CLI.EraBased.Run.Transaction
+import Cardano.CLI.Types.Common
+import Cardano.CLI.Types.Errors.TxCmdError
+import Cardano.Faucet.Misc (faucetValueToLovelace, getValue)
+import Cardano.Faucet.Types (FaucetValue, FaucetWebError (..))
 import Cardano.Faucet.Utils
 import Cardano.Prelude hiding ((%))
 import Control.Monad.Trans.Except.Extra (left)
-import Cardano.CLI.Types.Common
-import Cardano.CLI.EraBased.Run.Transaction
-import Cardano.CLI.Types.Errors.TxCmdError
 
 getMintedValue :: TxMintValue BuildTx era -> Value
 getMintedValue (TxMintValue _ val _) = val
@@ -18,17 +51,18 @@ getMintedValue TxMintNone = mempty
 
 newtype Fee = Fee L.Coin
 
-txBuild :: ()
-  => ShelleyBasedEra era
-  -> (TxIn, TxOut CtxUTxO era)
-  -> Either AddressAny [TxOutAnyEra]
-  -> TxCertificates BuildTx era
-  -> TxMintValue BuildTx era
-  -> Fee
-  -> ExceptT FaucetWebError IO (TxBody era)
+txBuild ::
+  () =>
+  ShelleyBasedEra era ->
+  (TxIn, TxOut CtxUTxO era) ->
+  Either AddressAny [TxOutAnyEra] ->
+  TxCertificates BuildTx era ->
+  TxMintValue BuildTx era ->
+  Fee ->
+  ExceptT FaucetWebError IO (TxBody era)
 txBuild sbe (txin, txout) addressOrOutputs certs minting (Fee fixedFee) = do
   let
-    --localNodeConnInfo = LocalNodeConnectInfo cModeParams networkId sockPath
+    -- localNodeConnInfo = LocalNodeConnectInfo cModeParams networkId sockPath
     unwrap :: TxOut ctx1 era1 -> FaucetValue
     unwrap (TxOut _ val _ _) = getValue val
     value :: L.Coin
@@ -41,98 +75,105 @@ txBuild sbe (txin, txout) addressOrOutputs certs minting (Fee fixedFee) = do
     changeValue = lovelaceToValue change <> mintedValue
 
     getTxOuts :: Either AddressAny [TxOutAnyEra] -> [TxOutAnyEra]
-    getTxOuts (Left addr) = [ TxOutAnyEra addr changeValue TxOutDatumByNone ReferenceScriptAnyEraNone ]
+    getTxOuts (Left addr) = [TxOutAnyEra addr changeValue TxOutDatumByNone ReferenceScriptAnyEraNone]
     getTxOuts (Right outs) = outs
 
-  txBodyContent <- TxBodyContent
-    <$> pure [(txin, BuildTxWith $ KeyWitness KeyWitnessForSpending)]
-    <*> pure TxInsCollateralNone
-    <*> pure TxInsReferenceNone
-    <*> mapM (\x -> withExceptT (FaucetWebErrorTodo . docToText . renderTxCmdError) $ toTxOutInAnyEra sbe x) (getTxOuts addressOrOutputs)
-    <*> pure TxTotalCollateralNone
-    <*> pure TxReturnCollateralNone
-    <*> validateTxFee sbe (Just fixedFee)
-    <*> pure TxValidityNoLowerBound
-    <*> pure (defaultTxValidityUpperBound sbe)
-    <*> pure TxMetadataNone
-    <*> pure TxAuxScriptsNone
-    <*> pure TxExtraKeyWitnessesNone
-    <*> pure (BuildTxWith Nothing)
-    <*> pure TxWithdrawalsNone
-    <*> pure certs
-    <*> pure TxUpdateProposalNone
-    <*> pure minting
-    <*> pure TxScriptValidityNone
-    <*> pure Nothing
-    <*> pure Nothing
-    <*> pure Nothing
-    <*> pure Nothing
+  txBodyContent <-
+    TxBodyContent
+      <$> pure [(txin, BuildTxWith $ KeyWitness KeyWitnessForSpending)]
+      <*> pure TxInsCollateralNone
+      <*> pure TxInsReferenceNone
+      <*> mapM
+        (\x -> withExceptT (FaucetWebErrorTodo . docToText . renderTxCmdError) $ toTxOutInAnyEra sbe x)
+        (getTxOuts addressOrOutputs)
+      <*> pure TxTotalCollateralNone
+      <*> pure TxReturnCollateralNone
+      <*> validateTxFee sbe (Just fixedFee)
+      <*> pure TxValidityNoLowerBound
+      <*> pure (defaultTxValidityUpperBound sbe)
+      <*> pure TxMetadataNone
+      <*> pure TxAuxScriptsNone
+      <*> pure TxExtraKeyWitnessesNone
+      <*> pure (BuildTxWith Nothing)
+      <*> pure TxWithdrawalsNone
+      <*> pure certs
+      <*> pure TxUpdateProposalNone
+      <*> pure minting
+      <*> pure TxScriptValidityNone
+      <*> pure Nothing
+      <*> pure Nothing
+      <*> pure Nothing
+      <*> pure Nothing
 
   case createAndValidateTransactionBody sbe txBodyContent of
     Left err -> left $ FaucetWebErrorTodo $ show err
     Right txbody -> pure txbody
-  {-
-   -- keep this code for now, as an example of how to use the cardano api in a monad
-  eInMode <- case toEraInMode era CardanoMode of
-    Just result -> return result
-    Nothing -> left (FaucetWebErrorConsensusModeMismatchTxBalance (show $ AnyConsensusMode CardanoMode) (AnyCardanoEra era))
 
-  let
-    utxo = UTxO $ Map.fromList [ (txin, txout) ]
+{-
+ -- keep this code for now, as an example of how to use the cardano api in a monad
+eInMode <- case toEraInMode era CardanoMode of
+  Just result -> return result
+  Nothing -> left (FaucetWebErrorConsensusModeMismatchTxBalance (show $ AnyConsensusMode CardanoMode) (AnyCardanoEra era))
 
-  (pparams, eraHistory, systemStart, stakePools) <-
-    newExceptT . fmap (join . first (FaucetWebErrorAcquireFailure . show)) $
-      executeLocalStateQueryExpr localNodeConnInfo Nothing $ \_ntcVersion -> runExceptT $ do
-        --UTxO utxo <- firstExceptT (_ . ShelleyTxCmdTxSubmitErrorEraMismatch) . newExceptT . queryExpr
-        --  $ QueryInEra eInMode $ QueryInShelleyBasedEra sbe
-        --  $ QueryUTxO (QueryUTxOByTxIn (Set.singleton txin))
+let
+  utxo = UTxO $ Map.fromList [ (txin, txout) ]
 
-        --when (null utxo || not (txin `L.elem` Map.keys utxo)) $ do
-          -- txout for txin does not exist
-        --  left $ ShelleyTxCmdTxInsDoNotExist [txin]
+(pparams, eraHistory, systemStart, stakePools) <-
+  newExceptT . fmap (join . first (FaucetWebErrorAcquireFailure . show)) $
+    executeLocalStateQueryExpr localNodeConnInfo Nothing $ \_ntcVersion -> runExceptT $ do
+      --UTxO utxo <- firstExceptT (_ . ShelleyTxCmdTxSubmitErrorEraMismatch) . newExceptT . queryExpr
+      --  $ QueryInEra eInMode $ QueryInShelleyBasedEra sbe
+      --  $ QueryUTxO (QueryUTxOByTxIn (Set.singleton txin))
 
-        pparams <- firstExceptT (FaucetWebErrorEraMismatch . show) . newExceptT . queryExpr
-          $ QueryInEra eInMode $ QueryInShelleyBasedEra sbe QueryProtocolParameters
+      --when (null utxo || not (txin `L.elem` Map.keys utxo)) $ do
+        -- txout for txin does not exist
+      --  left $ ShelleyTxCmdTxInsDoNotExist [txin]
 
-        eraHistory <- lift . queryExpr $ QueryEraHistory CardanoModeIsMultiEra
+      pparams <- firstExceptT (FaucetWebErrorEraMismatch . show) . newExceptT . queryExpr
+        $ QueryInEra eInMode $ QueryInShelleyBasedEra sbe QueryProtocolParameters
 
-        systemStart <- lift $ queryExpr QuerySystemStart
+      eraHistory <- lift . queryExpr $ QueryEraHistory CardanoModeIsMultiEra
 
-        stakePools <- firstExceptT (FaucetWebErrorEraMismatch . show) . ExceptT $
-          queryExpr . QueryInEra eInMode . QueryInShelleyBasedEra sbe $ QueryStakePools
+      systemStart <- lift $ queryExpr QuerySystemStart
 
-        return (pparams, eraHistory, systemStart, stakePools)
+      stakePools <- firstExceptT (FaucetWebErrorEraMismatch . show) . ExceptT $
+        queryExpr . QueryInEra eInMode . QueryInShelleyBasedEra sbe $ QueryStakePools
 
-  cAddr <- pure $ case anyAddressInEra era changeAddr of
-    Just addr -> addr
-    Nothing -> Prelude.error "txBuild: Byron address used: "
+      return (pparams, eraHistory, systemStart, stakePools)
 
-  (BalancedTxBody balancedTxBody _ _fee) <- firstExceptT (FaucetWebErrorAutoBalance . T.pack . displayError) . hoistEither $
-    makeTransactionBodyAutoBalance eInMode systemStart eraHistory pparams stakePools utxo txBodyContent cAddr Nothing
+cAddr <- pure $ case anyAddressInEra era changeAddr of
+  Just addr -> addr
+  Nothing -> Prelude.error "txBuild: Byron address used: "
 
-  return balancedTxBody
-  -}
+(BalancedTxBody balancedTxBody _ _fee) <- firstExceptT (FaucetWebErrorAutoBalance . T.pack . displayError) . hoistEither $
+  makeTransactionBodyAutoBalance eInMode systemStart eraHistory pparams stakePools utxo txBodyContent cAddr Nothing
 
-txSign :: ()
-  => ShelleyBasedEra era
-  -> TxBody era
-  -> [ShelleyWitnessSigningKey]
-  -> Tx era
+return balancedTxBody
+-}
+
+txSign ::
+  () =>
+  ShelleyBasedEra era ->
+  TxBody era ->
+  [ShelleyWitnessSigningKey] ->
+  Tx era
 txSign era txBody sks = tx
-  --let (sksByron, sksShelley) = partitionSomeWitnesses $ map categoriseSomeWitness sks
   where
+    -- let (sksByron, sksShelley) = partitionSomeWitnesses $ map categoriseSomeWitness sks
+
     shelleyKeyWitnesses = map (makeShelleyKeyWitness era txBody) sks
     tx = makeSignedTransaction shelleyKeyWitnesses txBody
 
-makeAndSignTx :: ()
-  => ShelleyBasedEra era
-  -> (TxIn, TxOut CtxUTxO era)
-  -> Either AddressAny [TxOutAnyEra]
-  -> [ShelleyWitnessSigningKey]
-  -> TxCertificates BuildTx era
-  -> TxMintValue BuildTx era
-  -> Fee
-  -> ExceptT FaucetWebError IO (Tx era, TxId)
+makeAndSignTx ::
+  () =>
+  ShelleyBasedEra era ->
+  (TxIn, TxOut CtxUTxO era) ->
+  Either AddressAny [TxOutAnyEra] ->
+  [ShelleyWitnessSigningKey] ->
+  TxCertificates BuildTx era ->
+  TxMintValue BuildTx era ->
+  Fee ->
+  ExceptT FaucetWebError IO (Tx era, TxId)
 makeAndSignTx sbe txinout addressOrOutputs skeys certs minting fee = do
   -- instead of having to specify an output that is exactly equal to input-fees
   -- i specify no outputs, and set the change addr to the end-user
--- cardano-faucet/src/Cardano/Faucet/Utils.hs
+++ cardano-faucet/src/Cardano/Faucet/Utils.hs
@@ -1,24 +1,38 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 module Cardano.Faucet.Utils where
 
-import Cardano.Api (toCardanoEra, TxIn, TxOut(TxOut), CtxUTxO, TxFee (..), defaultTxValidityUpperBound, TxValidityLowerBound(TxValidityNoLowerBound), TxValidityUpperBound, ShelleyBasedEra, AnyCardanoEra (..), shelleyBasedEraConstraints, Tx, CardanoEra (..))
-import qualified Cardano.Api.Ledger as L
-import Cardano.Api.Shelley (ShelleyBasedEra(..))
-import qualified Cardano.CLI.Json.Friendly as CLI
-import qualified Cardano.CLI.Types.MonadWarning as CLI
+import Cardano.Api (
+  AnyCardanoEra (..),
+  CardanoEra (..),
+  CtxUTxO,
+  ShelleyBasedEra,
+  Tx,
+  TxFee (..),
+  TxIn,
+  TxOut (TxOut),
+  TxValidityLowerBound (TxValidityNoLowerBound),
+  TxValidityUpperBound,
+  defaultTxValidityUpperBound,
+  shelleyBasedEraConstraints,
+  toCardanoEra,
+ )
+import Cardano.Api.Ledger qualified as L
+import Cardano.Api.Shelley (ShelleyBasedEra (..))
+import Cardano.CLI.Json.Friendly qualified as CLI
+import Cardano.CLI.Types.MonadWarning qualified as CLI
 import Cardano.Faucet.Misc
 import Cardano.Faucet.Types
 import Cardano.Prelude hiding ((%))
-import Control.Concurrent.STM (TMVar, takeTMVar, putTMVar)
+import Control.Concurrent.STM (TMVar, putTMVar, takeTMVar)
 import Control.Monad.Trans.Except.Extra (left)
-import qualified Data.ByteString as BS
+import Data.ByteString qualified as BS
 import Data.Map.Strict qualified as Map
-import qualified Prelude
+import Prelude qualified
 
 computeUtxoStats :: Map TxIn (TxOut CtxUTxO era) -> UtxoStats
 computeUtxoStats utxo = do
@@ -26,20 +40,22 @@ computeUtxoStats utxo = do
     convertValue :: TxOut ctx era -> FaucetValue
     -- TODO, also report tokens in this list/type
     convertValue (TxOut _ value _ _) = getValue value
-    --folder :: UtxoStats -> FaucetValue -> UtxoStats
-    --folder (UtxoStats m) v = UtxoStats $ Map.insert v ((fromMaybe 0 $ Map.lookup v m) + 1) m
+  -- folder :: UtxoStats -> FaucetValue -> UtxoStats
+  -- folder (UtxoStats m) v = UtxoStats $ Map.insert v ((fromMaybe 0 $ Map.lookup v m) + 1) m
   UtxoStats $ Map.fromList $ countLength $ sort $ map convertValue $ Map.elems utxo
-  --foldl' folder (UtxoStats mempty) $ concat $ map convertValue $ Map.elems utxo
+
+-- foldl' folder (UtxoStats mempty) $ concat $ map convertValue $ Map.elems utxo
 
 countDuplicates :: Ord a => [a] -> [(a, Int)]
 countDuplicates = map fn . group . sort
   where
-    fn (x:xs) = (x, 1 + length xs)
+    fn (x : xs) = (x, 1 + length xs)
     fn [] = Prelude.error "not possible"
 
 -- from https://hackage.haskell.org/package/polysemy-plugin-0.4.3.1/docs/Polysemy-Plugin-Fundep-Utils.html#v:countLength
+
 -- | Count the number of times 'a' is present in the list.
-countLength ::  Eq a => [a] -> [(a, Int)]
+countLength :: Eq a => [a] -> [(a, Int)]
 countLength as =
   let grouped = group as
    in zipWith (curry $ bimap Prelude.head Prelude.length) grouped grouped
@@ -62,42 +78,48 @@ takeOneUtxo utxoTMVar value = do
       putTMVar utxoTMVar utxo
       pure Nothing
 
-findUtxoOfSize :: TMVar (Map TxIn (TxOut CtxUTxO era)) -> FaucetValue -> STM (TxIn, TxOut CtxUTxO era)
+findUtxoOfSize ::
+  TMVar (Map TxIn (TxOut CtxUTxO era)) -> FaucetValue -> STM (TxIn, TxOut CtxUTxO era)
 findUtxoOfSize utxoTMVar value = do
   -- TODO, include fee
-  mTxinout <- {- liftIO $ atomically $ -} takeOneUtxo utxoTMVar value
+  mTxinout {- liftIO $ atomically $ -} <- takeOneUtxo utxoTMVar value
   case mTxinout of
     Just txinout -> pure txinout
     Nothing -> throwSTM $ FaucetWebErrorUtxoNotFound value
 
-validateTxFee :: ()
-  => ShelleyBasedEra era
-  -> Maybe L.Coin
-  -> ExceptT FaucetWebError IO (TxFee era)
+validateTxFee ::
+  () =>
+  ShelleyBasedEra era ->
+  Maybe L.Coin ->
+  ExceptT FaucetWebError IO (TxFee era)
 validateTxFee sbe mfee =
   case mfee of
     Nothing -> txFeatureMismatch sbe -- Fees are explicit since Shelley
     Just fee -> return $ TxFeeExplicit sbe fee
 
-txFeatureMismatch :: ()
-  => ShelleyBasedEra era
-  -> ExceptT FaucetWebError IO a
+txFeatureMismatch ::
+  () =>
+  ShelleyBasedEra era ->
+  ExceptT FaucetWebError IO a
 txFeatureMismatch sbe =
-   left $ FaucetWebErrorFeatureMismatch $
-     shelleyBasedEraConstraints sbe AnyCardanoEra $ toCardanoEra sbe
+  left
+    $ FaucetWebErrorFeatureMismatch
+    $ shelleyBasedEraConstraints sbe AnyCardanoEra
+    $ toCardanoEra sbe
 
 noBoundsIfSupported ::
-     ShelleyBasedEra era
-  -> (TxValidityLowerBound era, TxValidityUpperBound era)
+  ShelleyBasedEra era ->
+  (TxValidityLowerBound era, TxValidityUpperBound era)
 noBoundsIfSupported sbe = (TxValidityNoLowerBound, defaultTxValidityUpperBound sbe)
 
-prettyFriendlyTx :: ()
-  => ShelleyBasedEra era
-  -> Tx era
-  -> BS.ByteString
+prettyFriendlyTx ::
+  () =>
+  ShelleyBasedEra era ->
+  Tx era ->
+  BS.ByteString
 prettyFriendlyTx sbe tx =
-   CLI.friendlyBS CLI.FriendlyJson prettyTxAeson
-   where
+  CLI.friendlyBS CLI.FriendlyJson prettyTxAeson
+  where
     era = toCardanoEra sbe
     prettyTxAeson = fst $ runState (CLI.runWarningStateT $ CLI.friendlyTxImpl era tx) []
 
@@ -105,10 +127,10 @@ prettyFriendlyTx sbe tx =
 -- or returns an error message if the era is not Shelley based.
 cardanoEraToShelleyBasedEra :: CardanoEra era -> Either Text (ShelleyBasedEra era)
 cardanoEraToShelleyBasedEra = \case
-  ByronEra   -> Left "Byron is not a Shelley based era"
+  ByronEra -> Left "Byron is not a Shelley based era"
   ShelleyEra -> Right ShelleyBasedEraShelley
   AllegraEra -> Right ShelleyBasedEraAllegra
-  MaryEra    -> Right ShelleyBasedEraMary
-  AlonzoEra  -> Right ShelleyBasedEraAlonzo
+  MaryEra -> Right ShelleyBasedEraMary
+  AlonzoEra -> Right ShelleyBasedEraAlonzo
   BabbageEra -> Right ShelleyBasedEraBabbage
-  ConwayEra  -> Right ShelleyBasedEraConway
+  ConwayEra -> Right ShelleyBasedEraConway
