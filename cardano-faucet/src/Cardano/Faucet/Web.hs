aae28a3d8c6867d7482131a80293a933a9be7d24
John Lotoski <john.lotoski@iohk.io>
2026-08-17 21:57:06 -0500

    imp: log requestor ip for delegation errors

--- cardano-faucet/src/Cardano/Faucet/Web.hs
+++ cardano-faucet/src/Cardano/Faucet/Web.hs
@@ -496,6 +496,7 @@ handleDelegateStake
     let corsHeader = getCorsReply (fcfAllowedCorsOrigins fsConfig) mOrigin
     case eResult of
       Left err -> do
+        liftIO $ logError clientIP err
         pure $ corsHeader $ DelegationReplyError err
       Right result -> do
         pure $ corsHeader result
